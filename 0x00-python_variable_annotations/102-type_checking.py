a8163281b26b5c4bccdafa37e301a74b7f574603
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-10-08 10:25:05 +0100

    added 102-type_checking.py

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+using mypy to alidate the following piece of code 
+and apply any necessary changes.
+"""
+
+
+from typing import Tuple, List, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """validated types using mypy"""
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91),
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
